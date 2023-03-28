b89bef9858e49b549d37101414d84986636f59f7
aaha2134 <jacket.mar.jto.6874@gmail.com>
2023-03-28 15:46:54 +0900

    練習

--- app/controllers/protos_controller.rb
+++ app/controllers/protos_controller.rb
@@ -1,7 +1,13 @@
 class ProtosController < ApplicationController
+<<<<<<< Updated upstream
  def index
   @protos=Proto.all
  end
+=======
+  def index
+    @protos=Proto.includes(:user)
+  end
+>>>>>>> Stashed changes
 
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'protos#index'
+<<<<<<< Updated upstream
   resources :protos
+=======
+  resources :protos do
+    resources :comments, only: :create
+    
+  end
+  
+>>>>>>> Stashed changes
 end
